GeopJr/Tuba

commit 0a6fec2087cb368ef2debd565d81986c7ba5d3be

fix(Host): do not use sync methods
fix: #269

src/Utils/Host.vala
@@ -75,10 +75,21 @@ public class Tuba.Host {
 				.await ();
 
 			var data = msg.response_body;
-			FileOutputStream stream = file.create (FileCreateFlags.PRIVATE);
-			stream.splice (data, OutputStreamSpliceFlags.CLOSE_SOURCE | OutputStreamSpliceFlags.CLOSE_TARGET);
-
-			message (@"   OK: File written to: $file_path");
+			file.create_async.begin (FileCreateFlags.PRIVATE, Priority.DEFAULT, null, (obj, res) => {
+				try {
+					FileOutputStream stream = file.create_async.end (res);
+					stream.splice_async.begin (data, OutputStreamSpliceFlags.CLOSE_SOURCE | OutputStreamSpliceFlags.CLOSE_TARGET, Priority.DEFAULT, null, (obj2, res2) => {
+						try {
+							stream.splice_async.end (res2);
+							message (@"   OK: File written to: $file_path");
+						} catch (IOError e) {
+							warning (@"Couldn't the stream: $(e.message)");
+						}
+					});
+				} catch (Error e) {
+					warning (@"Couldn't create a file: $(e.message)");
+				}
+			});
 		}
 		else
 			message ("   OK: File already exists");

commit 7d821b88553a9fe83ae3d7514522430f22bf174a

fix(Host): yield
fix issue caused by 0a6fec2087cb368ef2debd565d81986c7ba5d3be

src/Utils/Host.vala
@@ -75,21 +75,10 @@ public class Tuba.Host {
 				.await ();
 
 			var data = msg.response_body;
-			file.create_async.begin (FileCreateFlags.PRIVATE, Priority.DEFAULT, null, (obj, res) => {
-				try {
-					FileOutputStream stream = file.create_async.end (res);
-					stream.splice_async.begin (data, OutputStreamSpliceFlags.CLOSE_SOURCE | OutputStreamSpliceFlags.CLOSE_TARGET, Priority.DEFAULT, null, (obj2, res2) => {
-						try {
-							stream.splice_async.end (res2);
-							message (@"   OK: File written to: $file_path");
-						} catch (IOError e) {
-							warning (@"Couldn't the stream: $(e.message)");
-						}
-					});
-				} catch (Error e) {
-					warning (@"Couldn't create a file: $(e.message)");
-				}
-			});
+			FileOutputStream stream = yield file.create_async (FileCreateFlags.PRIVATE);
+			yield stream.splice_async (data, OutputStreamSpliceFlags.CLOSE_SOURCE | OutputStreamSpliceFlags.CLOSE_TARGET);
+
+			message (@"   OK: File written to: $file_path");
 		}
 		else
 			message ("   OK: File already exists");