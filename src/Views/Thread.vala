GeopJr/Tuba

commit 90fffa6553af933740b258aab9aeea94541fa96d

fix(Thread): segfault (#338)

src/Views/Thread.vala
@@ -22,7 +22,7 @@ public class Tuba.Views.Thread : Views.ContentBase, AccountHolder {
 
 	public override void on_account_changed (InstanceAccount? acc) {
 		account = acc;
-		request ();
+		GLib.Idle.add (request);
 	}
 
 	void connect_threads () {
@@ -52,8 +52,10 @@ public class Tuba.Views.Thread : Views.ContentBase, AccountHolder {
 			w.content.selectable = true;
 		}
 
-		root_widget.thread_line_top.hide ();
-		root_widget.thread_line_bottom.hide ();
+		if (root_widget != null) {
+			root_widget.thread_line_top.hide ();
+			root_widget.thread_line_bottom.hide ();
+		}
 	}
 
 	private void on_replied (API.Status t_status) {
@@ -73,7 +75,7 @@ public class Tuba.Views.Thread : Views.ContentBase, AccountHolder {
 		connect_threads ();
 	}
 
-	public void request () {
+	public bool request () {
 		new Request.GET (@"/api/v1/statuses/$(root_status.id)/context")
 			.with_account (account)
 			.with_ctx (this)
@@ -92,8 +94,11 @@ public class Tuba.Views.Thread : Views.ContentBase, AccountHolder {
 				model.append (root_status);
 				uint root_index;
 				model.find (root_status, out root_index);
-				root_widget = content.get_row_at_index ((int)root_index) as Widgets.Status;
-				root_widget.expand_root ();
+
+				if (root_widget != null) {
+					root_widget = content.get_row_at_index ((int)root_index) as Widgets.Status;
+					root_widget.expand_root ();
+				}
 
 				Object[] to_add_descendants = {};
 				var descendants = root.get_array_member ("descendants");
@@ -112,6 +117,8 @@ public class Tuba.Views.Thread : Views.ContentBase, AccountHolder {
 				// scrolled.vadjustment.value = (double)(y*-1);
 			})
 			.exec ();
+
+		return GLib.Source.REMOVE;
 	}
 
 	public static void open_from_link (string q) {

commit 57b480b3dfd5a49f15e7ddcf35430b4d7165b092

fix(Thread): root_widget null check prior to setting it
regression from #338
fix: #366

src/Views/Thread.vala
@@ -95,8 +95,8 @@ public class Tuba.Views.Thread : Views.ContentBase, AccountHolder {
 				uint root_index;
 				model.find (root_status, out root_index);
 
+				root_widget = content.get_row_at_index ((int)root_index) as Widgets.Status;
 				if (root_widget != null) {
-					root_widget = content.get_row_at_index ((int)root_index) as Widgets.Status;
 					root_widget.expand_root ();
 				}
 